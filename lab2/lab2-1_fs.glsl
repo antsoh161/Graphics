9f5ad314635825c7d21b0276e33f601ee52576c4
Anton S <soderlund.anton@gmail.com>
2019-11-17 13:37:04 +0000

    shader started

--- lab2/lab2-1_fs.glsl
+++ lab2/lab2-1_fs.glsl
@@ -1,2 +1,16 @@
-// Build on your code from Lab 1 here
+#version 400
+in vec4 Normal;
+out vec4 frag_colour;
 
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+void main () {
+	//frag_colour = vec4(vec3(0.5,0.5,0.5)+1,0.5);
+	frag_colour.r = (Normal.x+0.5)*0.5;
+	//frag_colour = Normal;
+}
