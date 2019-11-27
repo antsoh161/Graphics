4e8265fd199e7692a33e84bef707a0630a8ed369
Anton S <soderlund.anton@gmail.com>
2019-11-27 21:47:07 +0100

    .

--- lab2/lab2-3_fs.glsl
+++ lab2/lab2-3_fs.glsl
@@ -4,7 +4,6 @@
 out vec4 frag_colour;
 in vec4 Normal;
 in vec4 position;
-in vec4 viewPosition;
 
 uniform int light_count;
 uniform vec4 light_position[4]; // can have up to 4 light sources
@@ -12,11 +11,12 @@ uniform vec4 light_colour[4];
 
 
 vec4 blinn_phong_brdf(vec4 in_direction, vec4 out_direction, vec4 normal){
-  float kL = 0.1;
+  float kL = 0;
   float kg = 0.9;
   float f = 10;
-  float N_dot_H = max(dot(normalize(Normal), out_direction),0.0);
+  float N_dot_H = max(dot(normalize(normal), normalize(out_direction)),0.0);
   float fr = kL/M_PI + (8 + f)/(8*M_PI)* kg * pow(N_dot_H,f);
+  
   return vec4(fr);
 }
 
@@ -24,16 +24,16 @@ void main () {
   vec4 objectColour = vec4(1, 0.8, 0.5, 0);
   vec4 ambient = 0.2 * objectColour;  
   vec4 viewDir = normalize(-position);
-  vec4 outSum;
+  vec4 outSum = vec4(0);
   
   for (int l = 0; l < light_count;++l){
     vec4 lightDir = normalize(light_position[l] - position);
-    vec4 H = normalize(lightDir + viewDir);
-    vec4 bp = blinn_phong_brdf(lightDir, H, Normal);
-  
-    outSum += light_colour[l] * bp * max(dot( normalize(Normal),lightDir),0.0);
+    vec4 H = normalize(lightDir+viewDir);
+    vec4 brdf = blinn_phong_brdf(lightDir, H, Normal);
+    vec4 a = vec4(3);
+    outSum += light_colour[l] * brdf * max(dot(lightDir, normalize(Normal)),0.0);
+    
   }
-  frag_colour = ambient + outSum;
+  frag_colour = outSum;
 }
-
 
--- lab2/lab2-3_vs.glsl
+++ lab2/lab2-3_vs.glsl
@@ -7,24 +7,21 @@ uniform mat4 view;
 
 out vec4 Normal;
 out vec4 position;
-out vec4 viewPosition;
 
 /*
 void main () {
 	
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------//
-// Apply the model, view and projection transform to vertex positions and forward the position to the fragment shader using an appropriate "out" variable
-//-----------------------------------------------------------------------------------------------------------------------------------------------------------//
   Normal = model*normals;
   position = model*vp;
-  viewPosition = view*model*vp;
+  viewPosition = model*vp;
   gl_Position = projection*view*model*vp;
+  
 };
  */
  
  void main() {
  	gl_Position = projection*view*model*vp;
-	position = view*vp;
-  position = position;
+  position = view*model*vp;
 	Normal = model*normals;
+  
  }
